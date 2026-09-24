395bc7b32080a95ed4b3cde1db29f73f473b40ee
Rohit <gohilrohit2727@gmail.com>
2026-09-24 12:17:02 +0530

    Add files via upload

---
+++ Numpy/arange function.py
@@ -0,0 +1,9 @@
+#arrange function
+
+from numpy import *
+a = arange(1,10,3)
+print(a)
+
+a = arange(10)
+print(a)
+
---
+++ Numpy/linesplace.py
@@ -0,0 +1,11 @@
+#LINSPACE
+
+from numpy import *
+a = linspace(1,5,5)
+print(a)
+
+a = linspace(0,10,5)
+print(a)
+
+a = linspace(1,10,4)
+print(a)
